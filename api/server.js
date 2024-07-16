903b8716382568a5f65e58294672afcec95b487c
Shubham Chaturvedi <shubhamchat224122@gmail.com>
2024-07-16 13:30:40 +0530

    debuggin building

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(
 	cors({
-		origin: "*",
+		origin: "http://localhost:5173",
 		credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
+
 axios.defaults.withCredentials = true;
 
 export const login = async (credentials) => {
