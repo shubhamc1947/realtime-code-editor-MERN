f9e676aa60f6621401ff3d6db14fd9606053ca57
Shubham Chaturvedi <shubhamchat224122@gmail.com>
2024-07-16 11:22:08 +0530

    added few bugs fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 axios.defaults.withCredentials = true;
 
