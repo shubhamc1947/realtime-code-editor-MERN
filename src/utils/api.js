e3e26be943d0785c52530c9650f7c8ea20a63419
Shubham Chaturvedi <shubhamchat224122@gmail.com>
2024-07-16 10:49:02 +0530

    added comment

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,10 +7,12 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 export const login = async (credentials) => {
+  console.log("login here is happneing testing text")
   const response = await axios.post(`${API_BASE_URL}/api/auth/login`, credentials);
   console.log(response.data)
   return response.data;
 };
+
 
 export const register = async (credentials) => {
   const response = await axios.post(`${API_BASE_URL}/api/auth/register`, credentials);
